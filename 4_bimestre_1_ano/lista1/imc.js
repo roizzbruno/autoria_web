a61ca65b6db2a1d686e8aadc9350398a371d93ab
roizzbruno <brunodufrfi@gmail.com>
2026-01-24 19:34:21 -0300

    Adicionando 9 questões da lista 1

--- 4_bimestre_1_ano/lista1/imc.js
+++
@@ -1,9 +0,0 @@
-function calcIMC(p, a){
-    let imc;
-    let = p / (a * a);
-    return imc.toFixed(1)
-}
-let p, a;
-p = parseFloat(prompt('Digite seu peso: '))
-a = parseFloat(prompt('Digite sua altura: '))
-alert('Seu IMC é: ' + calcIMC(p,a))
